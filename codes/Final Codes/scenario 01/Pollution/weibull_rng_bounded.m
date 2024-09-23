fc1512c51ba2fd3030f45838dc8649a602ccbbfa
Morteza <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-09-23 12:14:54 +0330

    Delete codes/Final Codes directory

--- codes/Final Codes/scenario 01/Pollution/weibull_rng_bounded.m
+++
@@ -1,6 +0,0 @@
-function weibull_random = weibull_rng_bounded(shape_parameter, scale_parameter, lower_bound, upper_bound)
-weibull_random = wblrnd(shape_parameter, scale_parameter);
-while weibull_random < lower_bound || weibull_random > upper_bound
-    weibull_random = wblrnd(shape_parameter, scale_parameter);
-end
-end
